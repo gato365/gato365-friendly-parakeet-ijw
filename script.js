0e3a6528091116942042a6c83a928e0e9e3dd00a
gato365 <immanuelwilliams@gato365.com>
2022-10-05 02:50:40 -0700

    Place psuedo code in program

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Assignment code here
 
 
+
+// Comment: Empty password
+// Str final Password =""
+
+//  From user
+// int number Of Digits =_
+// If (is Uppercase = = TRUE){
+// final Password = upper + final Password
+// }'
+// if (is Lowercase = = TRUE){
+// final Password = lower + final Password
+// }'
+// Éf (is special character = = TRUE){
+// final Password = special character 1- final Password
+// }'
+// if (is Numeric--= TRUE){
+// final Password = Numeric + final Password
+// }'
+// randomly select
+// number of Digits
+// from final Password
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
