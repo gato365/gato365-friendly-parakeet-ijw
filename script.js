3c11fd75c3f4cec17888afb325c67e930317bbe2
gato365 <immanuelwilliams@gato365.com>
2022-10-08 07:48:15 -0700

    Added correct special symbols

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //
 //
-// B. Create array of all possible chars
+// A. Create array of all possible chars
 //
 //
 
-// B.1 uppercase array - Found code to create uppercase alphabet - https://plainenglish.io/blog/create-an-array-of-alphabet-characters-in-javascript-with-this-simple-trick
+// A.1 uppercase array - Found code to create uppercase alphabet - https://plainenglish.io/blog/create-an-array-of-alphabet-characters-in-javascript-with-this-simple-trick
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const upper = alpha.map((x) => String.fromCharCode(x));
 
 
-// B.2 lowercase array - Found method to change upper into lower
+// A.2 lowercase array - Found method to change upper into lower
 //https://bobbyhadz.com/blog/javascript-convert-all-elements-of-array-to-lowercase#:~:text=To%20convert%20all%20array%20elements,array%20containing%20only%20lowercase%20strings.
 const lower = upper.map(element => {
   return element.toLowerCase();
@@ -17,81 +17,99 @@ const lower = upper.map(element => {
 
 
 
-// B.3 Special Characters created array from scracth
+// A.3 Special Characters created array from scracth
 //Issue 2: Get complete array of special character
-const specialCharacter = ['$', '@', '%', '!', '#', '%', '^', '&'];
+const specialCharacter =  ['"',' ','!','"',
+'#','$','%','&',
+'\'','(',')','*','+',
+',','-','.','/',':',';',
+'<','=','>','?','@','[',
+'\\',']','^','_','`','{','|','}','~','"'];
 
-
-// B.4 Numbers created array from scracth
+// A.4 Numbers created array from scracth
 //Issue 3: Get complete array of numbers
 const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
 //
-// A. Get Preferences from user
+// B. Get Preferences from user
 //
 //
 
-// A.1 User provides length of password
+// B.1 User provides length of password
 var slider = document.getElementById("myRange");
 var lengthOfOptionsfinalPP = document.getElementById("demo");
-//
-//  Problem 1: Why is this Telling HTML what to print
-//
 lengthOfOptionsfinalPP.innerHTML = slider.value;
-
-
-
-
-///
-// PROBLEM 2: What is this doing? Do I need it?
-
+// Display Slider in HTML
 slider.oninput = function () {
   lengthOfOptionsfinalPP.innerHTML = this.value;
 }
-
-
-
-// A.2 Determine whether user wants uppercase letters
+// B.2 Determine whether user wants uppercase letters
 var uppercaseCheckBox = document.getElementById("isUppercase");
-
-
-
-
-
-// A.3 Determine whether user wants lowercase letters
+// B.3 Determine whether user wants lowercase letters
 var lowercaseCheckBox = document.getElementById("isLowercase");
-
-
-// A.4 Determine whether user wants special characters
+// B.4 Determine whether user wants special characters
 var specialCharacterCheckBox = document.getElementById("isSpecialCharacter");
-
-// A.5 Determine whether user wants numeric
+// B.5 Determine whether user wants numeric
 var numericCheckBox = document.getElementById("isNumeric");
 
 
 
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// C. Generate Password Function
+// Name: generatePassword 
+// Purpose: Generate password based on specificaitons
+// Input: NA
+// Output: Password
+// Notes:  NA
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var generatePassword = function () {
 
-
   //
   //
-  // C. Add chars to array of password if user wants it
+  // C. Part i - Determine whether chars should be added based on user's input 
   //
   //
-
-  // Define empty password array
+  // C.i.0 Define empty password array
   var finalPasswordPossibilities = [];
-  if (uppercaseCheckBox.checked == true) { // C.1 Does user want uppercases chars
+  if (uppercaseCheckBox.checked == true) { // C.i.1 Does user want uppercases chars
     finalPasswordPossibilities = finalPasswordPossibilities.concat(upper);
   }
-  if (lowercaseCheckBox.checked == true) { // C.2 Does user want lowercases chars
+  if (lowercaseCheckBox.checked == true) { // C.i.2 Does user want lowercases chars
     finalPasswordPossibilities = finalPasswordPossibilities.concat(lower);
   }
-  if (specialCharacterCheckBox.checked == true) { // C.3 Does user want special chars
+  if (specialCharacterCheckBox.checked == true) { // C.i.3 Does user want special chars
     finalPasswordPossibilities = finalPasswordPossibilities.concat(specialCharacter);
   }
-  if (numericCheckBox.checked == true) { // C.4 Does user want numeric
+  if (numericCheckBox.checked == true) { // C.i.4 Does user want numeric
+    finalPasswordPossibilities = finalPasswordPossibilities.concat(numeric);
+  }
+
+  // If the user does not select any char type, we will include all types
+  if (uppercaseCheckBox.checked == false &
+    lowercaseCheckBox.checked == false &
+    specialCharacterCheckBox.checked == false &
+    numericCheckBox.checked == false) { // C.i.4 Does user want numeric
+    finalPasswordPossibilities = finalPasswordPossibilities.concat(upper);
+    finalPasswordPossibilities = finalPasswordPossibilities.concat(lower);
+    finalPasswordPossibilities = finalPasswordPossibilities.concat(specialCharacter);
     finalPasswordPossibilities = finalPasswordPossibilities.concat(numeric);
   }
 
@@ -100,55 +118,71 @@ var generatePassword = function () {
 
   //
   //
-  // D. Generate a Random Array of Numbers based on length of possibilities array
+  // C. Part ii - Generate a Random Array of Numbers based on length of possibilities array
   //
   //
 
-// Storing Length
-var lenFinalPassword = Number.parseInt(slider.value);
-console.log(lenFinalPassword)
+  // C.ii.1 Storing Length
+  var lenFinalPassword = Number.parseInt(slider.value);
 
-  // Issue 5: How do I make this give me random numbers?
+
+  // C.ii.2 Based on length of password and possble chars password is generated
   let result = new Array(lenFinalPassword)
-  var randomNumberIndices = result.fill(0).map(() => finalPasswordPossibilities [  Math.floor(Math.random() * finalPasswordPossibilities.length)] );
-  
+  var finalPW = result.fill(0).map(() => finalPasswordPossibilities[Math.floor(Math.random() * finalPasswordPossibilities.length)]);
 
-  //Issue 6: How do I make this work so that the random numbers get the indices of the array
-  
+  // C.ii.3 Removes commas from password and returns it
+  return finalPW.join('');
 
-
-  // Issue 7: Make above code a method
-
-
-  // Issue 8: How do I make incorporate this into the below function?
-  return randomNumberIndices.join('') ;
 };
 
 
 
 
-// Get references to the #generate element
+
+
+
+
+
+
+//
+//
+// D. React to Button Pressed
+//
+//
+
+// D.1 Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// D2.
+// Name: checkBoxFunction 
+// Purpose: Ability to get inforamtion from checkbox
+// Input: info from reader
+// Output: NA
+// Notes:  NA
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function checkBoxFunction(info) {
   var checkBox = document.getElementById(info);
-  // var text = document.getElementById("text");
-
 }
-
-
-
-
-
-// Write password to the #password input
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// D3. 
+// Name: writePassword
+// Purpose: Ability to write password back to html
+// Input: NA
+// Output: NA
+// Notes:  NA
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function writePassword() {
+  // Use function to generate password
   var password = generatePassword();
+  // Write password to the #password input
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
+// D4.  Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
